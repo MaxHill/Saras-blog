084d818ef67d786fca298bfd074ef82a7e601541
Max Hill <max.hill@hiq.se>
2020-01-28 13:07:44 +0100

    Debugging preview window styles

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,17 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { IndexPageTemplate } from "../../templates/index-page";
+import { StyleSheetManager } from "styled-components";
 
 const IndexPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(["data"]).toJS();
+  const iframe = document.getElementsByTagName("iframe")[0];
+  const iframeHeadElem = iframe.contentDocument.head;
 
   if (data) {
     return (
-      <IndexPageTemplate
-        heading={data.heading}
-        description={data.description}
-        email={data.email}
-      />
+      <StyleSheetManager target={iframeHeadElem}>
+        <IndexPageTemplate
+          heading={data.heading}
+          description={data.description}
+          email={data.email}
+        />
+      </StyleSheetManager>
     );
   } else {
     return <div>Loading...</div>;
