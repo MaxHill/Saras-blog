2436fefd18609ffe31a1a3dff91ffb9a5b3618d1
Max Hill <max.hill@hiq.se>
2020-01-30 15:51:20 +0100

    Remove duplicate font loading

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -10,9 +10,6 @@ import IndexPagePreview from "./preview-templates/IndexPagePreview";
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewStyle(
-  `https://fonts.googleapis.com/css?family=Raleway|Roboto+Slab&display=swap`
-);
 CMS.registerPreviewStyle(`/admin/cms.css`);
 
 CMS.registerPreviewTemplate("index", withStyled(IndexPagePreview));
