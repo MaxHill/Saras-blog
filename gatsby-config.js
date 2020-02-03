9586166716472c21e2dde2e920cf109bacf427ab
Max Hill <max.hill@hiq.se>
2020-02-03 14:22:54 +0100

    Updated site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: "Sara Hill",
-        short_name: "SH",
+        short_name: "Sara",
         start_url: "/",
         background_color: "#fff",
         theme_color: "#fff",
