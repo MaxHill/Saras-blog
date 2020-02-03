95ae983e8e4f8921383c7cbb14988bebcecbfb16
Max Hill <max.hill@hiq.se>
2020-02-03 15:09:42 +0100

    Fetch siteUrl with use site metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,77 +1,60 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import useSiteMetadata from "./SiteMetadata";
-import { withPrefix, StaticQuery } from "gatsby";
+import { withPrefix } from "gatsby";
 import { Normalize } from "styled-normalize";
 import GlobalStyle from "../styles/GlobalStyle";
 
 const TemplateWrapper = ({ children }) => {
-  const { title, description } = useSiteMetadata();
+  const { title, description, siteUrl } = useSiteMetadata();
   return (
-    <StaticQuery
-      query={graphql`
-        query SiteMetadata {
-          site {
-            siteMetadata {
-              siteUrl
-            }
-          }
-        }
-      `}
-      render={({
-        site: {
-          siteMetadata: { siteUrl }
-        }
-      }) => (
-        <>
-          <Normalize />
-          <GlobalStyle />
-          <Helmet>
-            <html lang="sv" />
-            <title>{title}</title>
-            <meta name="description" content={description} />
+    <>
+      <Normalize />
+      <GlobalStyle />
+      <Helmet>
+        <html lang="sv" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
 
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href={`${withPrefix("/")}img/apple-touch-icon.png`}
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href={`${withPrefix("/")}img/favicon-32x32.png`}
-              sizes="32x32"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href={`${withPrefix("/")}img/favicon-16x16.png`}
-              sizes="16x16"
-            />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${withPrefix("/")}img/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${withPrefix("/")}img/favicon-32x32.png`}
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${withPrefix("/")}img/favicon-16x16.png`}
+          sizes="16x16"
+        />
 
-            <link
-              rel="mask-icon"
-              href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
-              color="#fff"
-            />
+        <link
+          rel="mask-icon"
+          href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
+          color="#fff"
+        />
 
-            <meta name="msapplication-TileColor" content="#00aba9" />
-            <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
 
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={title} />
-            <meta property="og:url" content={siteUrl} />
-            <meta
-              property="og:image"
-              content={`${siteUrl}${withPrefix("/")}img/og-image.jpg`}
-            />
-            <meta property="og:image:width" content="279" />
-            <meta property="og:image:height" content="279" />
-          </Helmet>
-          {children}
-        </>
-      )}
-    />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={siteUrl} />
+        <meta
+          property="og:image"
+          content={`${siteUrl}${withPrefix("/")}img/og-image.jpg`}
+        />
+        <meta property="og:image:width" content="279" />
+        <meta property="og:image:height" content="279" />
+      </Helmet>
+      {children}
+    </>
   );
 };
 
